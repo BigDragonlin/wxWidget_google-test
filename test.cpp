644598d5e2aa03e5c34b04908123af211a58d1b3
草蜢 <1363992060@qq.com>
2024-10-22 10:58:03 +0800

    Add subtraction function and tests
    Added a new `Sub` function for subtraction along with corresponding test cases, while removing the addition test cases. The main function remains unchanged to continue running all tests.

--- test.cpp
+++ test.cpp
@@ -1,21 +1,22 @@
 #include <gtest/gtest.h>
 
 int Add(int a, int b) {
-    return a + b;
+	return a + b;
 }
 
-
-TEST(AdditionTest, HandlesPositiveNumbers) {
-    EXPECT_EQ(Add(1, 2), 3);  // 期待 Add(1, 2) 返回 3
-    EXPECT_EQ(Add(5, 7), 12);
+int Sub(int a, int b)
+{
+	return a - b;
 }
 
-TEST(AdditionTest, HandlesNegativeNumbers) {
-    EXPECT_EQ(Add(-1, -1), -2);  // 期待 Add(-1, -1) 返回 -2
-    EXPECT_EQ(Add(-5, 3), -2);
+TEST(SubTest, HandleSubtiveNumber)
+{
+	EXPECT_EQ(Sub(2, 1), 1);
+	EXPECT_EQ(Sub(5, 3), 2);
+	EXPECT_EQ(Sub(1, 1), 2);
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();  // 运行所有测试
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();  // 运行所有测试
 }
