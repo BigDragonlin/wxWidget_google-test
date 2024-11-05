75258213a54716f8c5febd87478b22c62bd2e062
草蜢 <1363992060@qq.com>
2024-11-05 17:19:31 +0800

    init

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,15 +1,27 @@
 #include <gtest/gtest.h>
-int Sub(int a, int b)
-{
-    return a - b;
-}
+#include <wx/wx.h>
 
-TEST(SubTest, HandleSubtiveNumber)
+class YourApp : public wxFrame
 {
-EXPECT_EQ(Sub(2, 1), 1);
-EXPECT_EQ(Sub(5, 3), 2);
-}
+public:
+    YourApp(const wxString& title);
+};
 
+YourApp::YourApp(const wxString& title)
+        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600))
+{
+    // 初始化代码，例如添加控件
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Click Me");
+    sizer->Add(button, 0, wxALL | wxCENTER, 10);
+    panel->SetSizer(sizer);
+}
+//测试wxx.cpp
+TEST(WxxTest, HandleWxx){
+    YourApp *frame = new YourApp("Hello wxWidgets");
+    frame->Show(true);
+}
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();  // 运行所有测试
