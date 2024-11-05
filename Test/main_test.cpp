2dc877d9ef7a63bbe490315e1705642b68f7ff58
草蜢 <1363992060@qq.com>
2024-11-05 16:04:21 +0800

    init googletest

---
+++ Test/main_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+int Sub(int a, int b)
+{
+    return a - b;
+}
+
+TEST(SubTest, HandleSubtiveNumber)
+{
+EXPECT_EQ(Sub(2, 1), 1);
+EXPECT_EQ(Sub(5, 3), 2);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();  // 运行所有测试
+}
--- readme.md
+++
@@ -1,8 +0,0 @@
-## install google-test
-```shell
-#ubuntu
-sudo apt-get install libgtest-dev
-
-#使用FetchContest直接cmake下载google-test。
-```
-
